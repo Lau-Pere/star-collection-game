533554070c5fce2114f62d26674a5b7418eb5ecb
Laura <pereyra.laura.a@gmail.com>
2025-06-08 01:13:51 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GalleryComponent } from './gallery/gallery.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: GalleryComponent
-  }
-];
